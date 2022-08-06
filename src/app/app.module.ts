4602ecce6cbc9cfdb748339b5af5dd82c670be72
ahmedfouad01099 <ahmedfouad01099@gmail.com>
2022-08-06 21:20:12 +0200

    finish products & cart pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CartComponent,
+    ProductComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
+import { CartService } from 'src/app/service/cart.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent implements OnInit {
+  public productList: any;
+  constructor(private api: ApiService, private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.api.getProducts().subscribe((res) => {
+      this.productList = res;
+
+      this.productList.forEach((item: any) => {
+        Object.assign(item, { quantity: 1, total: item.price });
+      });
+    });
+  }
+
+  addToCart(item: any) {
+    this.cartService.addToCart(item);
+  }
+}
